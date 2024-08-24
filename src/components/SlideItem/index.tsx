3ae16de4bd3a1513ef57f28861a16dad183243b3
김병민 <kbm5553@gnifrix.com>
2024-08-25 01:20:54 +0900

    add slide

--- src/components/SlideItem/index.tsx
+++ src/components/SlideItem/index.tsx
@@ -1,6 +1,7 @@
-import './Short.css';
+import React from 'react';
+import './slideitem.css';
 
-export default function Short() {
+const SlideItem: React.FC = () => {
   return (
     <div className="short">
       <div className="background"></div>
@@ -9,11 +10,23 @@ export default function Short() {
         <div className="container-1">
           <div className="dynamic-island"></div>
           <div className="status-pro">
-            <img className="cellular" src="assets/vectors/Cellular1_x2.svg" />
-            <img className="wifi" src="assets/vectors/Wifi2_x2.svg" />
+            <img
+              className="cellular"
+              src="assets/vectors/Cellular1_x2.svg"
+              alt="Cellular"
+            />
+            <img
+              className="wifi"
+              src="assets/vectors/Wifi2_x2.svg"
+              alt="Wifi"
+            />
             <div className="battery-percentage">
               <div className="percent"></div>
-              <img className="battery" src="assets/vectors/Battery2_x2.svg" />
+              <img
+                className="battery"
+                src="assets/vectors/Battery2_x2.svg"
+                alt="Battery"
+              />
             </div>
           </div>
         </div>
@@ -21,26 +34,43 @@ export default function Short() {
       <div className="container">
         <div className="group-5">
           <span className="container-1">외모지상주의. 1, 대원앤북</span>
-          <img className="play-arrow-1" src="assets/vectors/PlayArrow_x2.svg" />
+          <img
+            className="play-arrow-1"
+            src="assets/vectors/PlayArrow_x2.svg"
+            alt="Play"
+          />
         </div>
         <div className="button">
           <div className="up">
-            <img className="thumb-up-1" src="assets/vectors/ThumbUp_x2.svg" />
+            <img
+              className="thumb-up-1"
+              src="assets/vectors/ThumbUp_x2.svg"
+              alt="Thumb up"
+            />
             <span className="likes">Likes</span>
           </div>
           <div className="comment">
             <img
               className="chat-bubble-1"
               src="assets/vectors/ChatBubble_x2.svg"
+              alt="Chat bubble"
             />
             <span className="container">229</span>
           </div>
           <div className="export">
-            <img className="ios-share-1" src="assets/vectors/IosShare_x2.svg" />
+            <img
+              className="ios-share-1"
+              src="assets/vectors/IosShare_x2.svg"
+              alt="Share"
+            />
             <span className="export-1">Export</span>
           </div>
           <div className="menu-1">
-            <img className="more-vert-1" src="assets/vectors/MoreVert_x2.svg" />
+            <img
+              className="more-vert-1"
+              src="assets/vectors/MoreVert_x2.svg"
+              alt="More"
+            />
           </div>
         </div>
       </div>
@@ -50,18 +80,31 @@ export default function Short() {
       <div className="menu-2">
         <div className="frame-1">
           <div className="group-1">
-            <img className="home-1" src="assets/vectors/Home1_x2.svg" />
+            <img
+              className="home-1"
+              src="assets/vectors/Home1_x2.svg"
+              alt="Home"
+            />
             <span className="home-2">Home</span>
           </div>
           <div className="group-2">
-            <img className="search" src="assets/vectors/Search_x2.svg" />
+            <img
+              className="search"
+              src="assets/vectors/Search_x2.svg"
+              alt="Search"
+            />
             <span className="search-2">Search</span>
           </div>
-          <img className="add-circle-1" src="assets/vectors/AddCircle_x2.svg" />
+          <img
+            className="add-circle-1"
+            src="assets/vectors/AddCircle_x2.svg"
+            alt="Add"
+          />
           <div className="group-3">
             <img
               className="account-circle-1"
               src="assets/vectors/AccountCircle2_x2.svg"
+              alt="Account"
             />
             <span className="me">Me</span>
           </div>
@@ -69,6 +112,7 @@ export default function Short() {
             <img
               className="account-circle-2"
               src="assets/vectors/AccountCircle_x2.svg"
+              alt="Account"
             />
             <span className="me-1">Me</span>
           </div>
@@ -77,4 +121,6 @@ export default function Short() {
       </div>
     </div>
   );
-}
+};
+
+export default SlideItem;
--- src/pages/ShowPang/ShowPangPage.tsx
+++ src/pages/ShowPang/ShowPangPage.tsx
@@ -1,11 +1,23 @@
+import React from 'react';
+
+import Layout from '@/components/Layout.tsx';
+// import Menu from '@/components/Menu.tsx';
 import VerticalSlider from '@/components/Slide';
+import SlideItem from '@/components/SlideItem';
 
-const slides = [
-  <div key="1">슬라이드 1</div>,
-  <div key="2">슬라이드 2</div>,
-  <div key="3">슬라이드 3</div>,
-];
+export const ShowPangPage: React.FC = () => {
+  // 원하는 슬라이드 아이템의 개수를 정의합니다.
+  const numberOfSlides = 5;
 
-export const ShowPangPage = () => {
-  return <VerticalSlider slides={slides} />;
+  // SlideItem 컴포넌트를 여러 개 포함하는 배열을 생성합니다.
+  const slides = Array(numberOfSlides)
+    .fill(null)
+    .map((_, index) => <SlideItem key={index} />);
+
+  return (
+    <Layout>
+      <VerticalSlider slides={slides} />
+      {/* <Menu /> */}
+    </Layout>
+  );
 };
